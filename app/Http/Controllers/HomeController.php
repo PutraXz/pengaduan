9cf12721b2f735a89747ba776a0b53c36e69af15
PutraXz <ytprem200100@gmail.com>
2022-11-17 15:24:24 +0700

    Last to

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
         // $email = auth()->user()->name;
         // dd($email);
         
-        return view('admin.index');
+        return view('admin.home');
         
         
         
